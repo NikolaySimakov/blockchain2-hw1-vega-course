cbd0936bc2ff10a9c5722fa72e1a05bbcbff31ab
NikolaySimakov <n.simakoff@gmail.com>
2025-03-09 23:32:06 +0300

    fix: imports

--- test/VegaVote.t.sol
+++ test/VegaVote.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "forge-std/Test.sol";
+import "lib/forge-std/Test.sol";
 import "../src/VegaVote.sol";
 import "../src/VegaVoteNFT.sol";
 
